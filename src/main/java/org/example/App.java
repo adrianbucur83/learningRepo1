668e84f71aca67e321eb16a6a8ab60176291476b
adrianbucur83 <adrianbucur83@gmail.com>
2023-03-01 18:04:32 +0000

    first files in my project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import com.google.common.collect.Ordering;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
